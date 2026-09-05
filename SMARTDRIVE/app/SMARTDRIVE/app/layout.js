fac3b218269fc9b37aa6dec0c55cef46c98719aa
thomasplantier56-hub <thomasplantier56@gmail.com>
2026-09-06 00:42:53 +0200

    Add files via upload

---
+++ SMARTDRIVE/app/SMARTDRIVE/app/layout.js
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: 'SmartDrive',
+  description: 'Notre assistant de courses intelligent',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
